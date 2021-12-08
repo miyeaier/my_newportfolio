c90a427f9e6eafdaede3b00eacfd570026346bd5
miyeaier <miyesier@gmail.com>
2021-12-08 14:23:58 +0100

    fix mistake  add code Hellojsx and change header.jsx and add header my portfolio

--- my_portfolio/src/Header.jsx
+++ my_portfolio/src/Header.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-const Hello = () => {
+const Header = () => {
   return (
-    <div>
-      <h1 id="hello">Hello World</h1>
-    </div>
+    <nav>
+      <h1 id="header">My portfolio</h1>
+    </nav>
   );
 };
 
-export default Hello;
+export default Header;
