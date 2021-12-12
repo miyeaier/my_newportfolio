887e7f895925efc837e2f075736416b5d7915c9c
miyeaier <miyesier@gmail.com>
2021-12-12 21:11:54 +0100

    fix typ error and add axios and data folder and projects.json

--- my_portfolio/src/Component/Projects.jsx
+++ my_portfolio/src/Component/Projects.jsx
@@ -1,13 +1,36 @@
 import React from "react";
 import { Container } from "semantic-ui-react";
 
-const Project = () => {
-  return (
-    <Container>
-      <h1 id="projects-header">My Projects</h1>
-      <p>yoyo</p>
-    </Container>
-  );
-};
+class Projects extends Component {
+  state = {
+    projects: [
+      {
+        id: 1,
+        name: "My Frist Website",
+      },
+      {
+        id: 2,
+        name: "Fizzbuzz",
+      },
+    ],
+  };
+  reander() {
+    const { projects } = this.state;
 
-export default Project;
+    let projectsList = projects.map((project) => {
+      return(
+        <div id={`project-${project.id}`} key={project.id}>
+          <h3>{project.name}</h3>
+        </div>
+      );
+    });
+    return (
+      <Container>
+        <h1 id="projects-header">My Projects</h1>
+        {projectsList}
+      </Container>
+    );
+  }
+}
+
+export default Projects;
