054f2b631e5a712be8f7af80cad2637843ce3b31
miyeaier <miyesier@gmail.com>
2021-12-13 21:12:57 +0100

    stylied bit add background imag

--- my_portfolio/src/Component/Hello.jsx
+++ my_portfolio/src/Component/Hello.jsx
@@ -9,4 +9,6 @@ const Hello = () => {
   );
 };
 
+document.body.style.backgroundImage =
+  "url('https://images.unsplash.com/photo-1600196302476-1790ba2334f5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80')";
 export default Hello;
