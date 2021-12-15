d6ca4b1faf3353f7f14e349ea7d6573c324c55c7
miyeaier <miyesier@gmail.com>
2021-12-15 22:11:47 +0100

    change h1 and add new image

--- src/Component/Hello.jsx
+++ src/Component/Hello.jsx
@@ -5,12 +5,6 @@ const Hello = () => {
     <div className="ui main container">
       <div className="GeeksForGeeks">
         <h1>Hey There</h1>
-        <p1>
-          if you want to know more about me click About me and projects...{" "}
-        </p1>
-        <h2>
-          <i class="handshake outline icon"></i>
-        </h2>
       </div>
     </div>
   );
